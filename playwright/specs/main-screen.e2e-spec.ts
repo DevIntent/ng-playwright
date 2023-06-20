98f793d0ee2b13798ae80de4541ca4b558e9afce
Marcin Wosinek <marcin.wosinek@gmail.com>
2023-06-20 17:36:02 +0200

    Add simple e2e

--- playwright/specs/main-screen.e2e-spec.ts
+++ playwright/specs/main-screen.e2e-spec.ts
@@ -1,11 +1,11 @@
-import {expect, createTest} from '@ngx-playwright/test';
+import { expect, createTest } from '@ngx-playwright/test';
 
-import {MainScreen} from '../screens/main-screen.js';
+import { MainScreen } from '../screens/main-screen.js';
 
 const test = createTest(MainScreen);
 
 test.describe('the main screen of the application', () => {
-  test('it should have a title', ({$: {title}}) => {
-    expect(title).toBeTruthy();
+  test('it should have a title', async ({ $: { header } }) => {
+    expect(await header.text()).toEqual('Playwright e2e example');
   });
 });
